49047e33c2daebd3ba14a5a7e037e0d63dc7f7f9
Ekaluk52003 <ekaluk.p@gmail.com>
2021-08-03 19:28:57 +0700

    Remove node_modules folder

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const fs = require("fs");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+fs.readdirSync("./routes").map((r) =>
+  app.use("/api", require(`./routes/${r}`))
+);
+
+//make static folder
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+// redirect to file
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
+
+// Get all Restaurants
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
