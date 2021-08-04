f6d04eeaffc3ad92d3a267d23f14b313f9d7d0f2
Ekaluk52003 <ekaluk.p@gmail.com>
2021-08-04 20:01:14 +0700

    update server

--- server.compiled.js
+++ server.compiled.js
@@ -14,6 +14,8 @@ var morgan = require("morgan");
 
 var cookieParser = require("cookie-parser");
 
+var _process$env$port = process.env.port,
+    port = _process$env$port === void 0 ? 3000 : _process$env$port;
 var app = express();
 app.use(cors());
 app.use(express.json());
@@ -28,8 +30,8 @@ app.use(express["static"](path.join(__dirname, "/client/build"))); // redirect t
 app.get("*", function (req, res) {
   return res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 }); // Get all Restaurants
+// const port = process.env.PORT || 3001;
 
-var port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log("server is up and listening on port ".concat(port));
 });
